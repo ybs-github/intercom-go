bffbd301af1a580207be06fb69c9f13653cea6d2
Andrii Skoryk <andriiskoryk@Andriis-MacBook-Pro.local>
2020-10-26 20:24:49 +0200

    Add role to user model

--- user.go
+++ user.go
@@ -20,6 +20,7 @@ type UserList struct {
 type User struct {
 	ID                     string                 `json:"id,omitempty"`
 	Email                  string                 `json:"email,omitempty"`
+	Role                   string                 `json:"role,omitempty"`
 	Phone                  string                 `json:"phone,omitempty"`
 	UserID                 string                 `json:"user_id,omitempty"`
 	Anonymous              *bool                  `json:"anonymous,omitempty"`
